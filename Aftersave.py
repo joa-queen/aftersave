86cf803bccfa1ebbdddf0ed3de83da0454dc2e04
Joaquín Miguez <joacodeviaje@gmail.com>
2013-06-21 18:42:42 -0300

    first commit

---
+++ Aftersave.py
@@ -0,0 +1,20 @@
+import sublime, sublime_plugin, subprocess
+
+class UglifyCommand(sublime_plugin.TextCommand):
+	def run(self, text):
+		trigger = "aftersave:"
+		fl = self.view.substr(self.view.find("^(.+)\n", 0)).replace('/*', '').replace('*/', '').replace('//', '').replace('#', '').strip()
+		if (fl[0:len(trigger)] == trigger):
+			filename = self.view.file_name()
+			cwd = self.view.file_name().split('/')[:-1]
+			cwd = '/'.join(cwd)
+			command = fl.replace(trigger, '').replace('<self>', filename).strip()
+			p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
+			out, stderr = p.communicate()
+
+			if (stderr):
+				print 'Aftersave error: ' + stderr
+
+class UglifySave(sublime_plugin.EventListener):
+  def on_post_save(self, view):
+    view.run_command("uglify")
